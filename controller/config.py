256f83652f9f0d3dfa6417ef32d6fd8a45af0004
andyt <andyt05@gmail.com>
2012-05-15 20:49:13 -0500

    Adds DefaultConfig borg object for reading/writing config.

--- controller/config.py
+++ controller/config.py
@@ -1,10 +1,29 @@
 import os
-from ConfigParser import ConfigParser
+from ConfigParser import SafeConfigParser
+from log import get_log
+
+log = get_log("config")
+filename = "%s/default.cfg" % os.path.realpath(os.path.dirname(__file__))
+
+# A Borg config class so that all configuration objects are the same
+# when configuration values change
+class DefaultConfig(SafeConfigParser):
+    __shared_state = {}
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+        SafeConfigParser.__init__(self)
+
+    def save(self):
+        f = open(filename, 'w')
+        try:
+            self.write(f)
+        except:
+            log.exception()
+        finally:
+            f.close()
 
 def get_config():
-    cfgfilename = "%s/default.cfg" % os.path.realpath(
-            os.path.dirname(__file__))
-    config = ConfigParser()
-    config.read(cfgfilename)
+    config = DefaultConfig()
+    config.read(filename)
     return config
 
