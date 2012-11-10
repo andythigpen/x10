a5517822931881f09a3a0c76bdb03ad759b3f2a9
andyt <andyt05@gmail.com>
2012-11-09 21:20:49 -0600

    Fix ajax caching issue on iOS.

--- controller/static/js/app.js
+++ controller/static/js/app.js
@@ -2,7 +2,7 @@
 var updateUiTimer = null;
 function enableUiUpdate() {
     updateUiTimer = setInterval(function() {
-        $.getJSON('/status', loadStatus);
+        $.getJSON('/status', {"ts":new Date().getTime()}, loadStatus);
     }, 3000);
 }
 
@@ -67,26 +67,38 @@ $(document).ready(function() {
     /** Lights */
     $("#light-switch").change(function() {
         disableUiUpdate();
-        $.post('/lights', {"action": $(this).val()}, loadLightsStatus);
+        $.post('/lights', {
+            "action" : $(this).val(),
+            "ts"     : new Date().getTime()
+        }, loadLightsStatus);
     });
 
     $("#bright-btn").click(function() {
         disableUiUpdate();
-        $.post('/lights', {"action": "bright"}, loadLightsStatus);
+        $.post('/lights', {
+            "action" : "bright",
+            "ts"     : new Date().getTime()
+        }, loadLightsStatus);
     });
 
     $("#dim-btn").click(function() {
         disableUiUpdate();
-        $.post('/lights', {"action": "dim"}, loadLightsStatus);
+        $.post('/lights', {
+            "action" : "dim",
+            "ts"     : new Date().getTime()
+        }, loadLightsStatus);
     });
 
     $("#scene-menu").change(function() {
         if ($(this).val() != "-") {
             disableUiUpdate();
-            $.post('/lights', {"action": "scene", "scene":$(this).val()}, 
-                function(data) {
-                    $("#scene-menu").val("-").selectmenu('refresh');
-                    loadLightsStatus(data);
+            $.post('/lights', {
+                "action" : "scene",
+                "scene"  : $(this).val(),
+                "ts"     : new Date().getTime()
+            }, function(data) {
+                $("#scene-menu").val("-").selectmenu('refresh');
+                loadLightsStatus(data);
             });
         }
     });
@@ -94,8 +106,11 @@ $(document).ready(function() {
     /** Programs */
     $("#xbmc-switch").change(function() {
         disableUiUpdate();
-        $.post('/programs', {"action": $(this).val(), "program":"xbmc"}, 
-            loadProgramsStatus);
+        $.post('/programs', {
+            "action"  : $(this).val(),
+            "program" : "xbmc",
+            "ts"      : new Date().getTime()
+        }, loadProgramsStatus);
     });
 
     // this allows the voice dictation on iOS to work when the Done button
@@ -106,7 +121,10 @@ $(document).ready(function() {
         }
         var sequence = "str "+$(this).val()+"\n";
         $(this).val('');
-        $.post('/keyboard', {"sequence":sequence});
+        $.post('/keyboard', {
+            "sequence" : sequence,
+            "ts"       : new Date().getTime()
+        });
     });
 
     // send keyboard events through to xte
@@ -121,7 +139,10 @@ $(document).ready(function() {
             case 13: sequence = "key Return\n"; break;
             case 32: sequence = "key Space\n"; break;
         }
-        $.post('/keyboard', {"sequence":sequence});
+        $.post('/keyboard', {
+            "sequence" : sequence,
+            "ts"       : new Date().getTime()
+        });
     });
 
     $("#shutdown-btn").click(function() {
@@ -131,8 +152,12 @@ $(document).ready(function() {
             ! confirm("Are you sure you want to shutdown now?")) {
             return;
         }
-        $.post('/programs', {"action":"shutdown", "program":"pc", "when":when}, 
-            loadProgramsStatus);
+        $.post('/programs', {
+            "action"  : "shutdown",
+            "program" : "pc",
+            "when"    : when,
+            "ts"      : new Date().getTime()
+        }, loadProgramsStatus);
     });
 
     $("#restart-btn").click(function() {
@@ -142,8 +167,12 @@ $(document).ready(function() {
             ! confirm("Are you sure you want to restart now?")) {
             return;
         }
-        $.post('/programs', {"action":"restart", "program":"pc", "when":when}, 
-            loadProgramsStatus);
+        $.post('/programs', {
+            "action"  : "restart",
+            "program" : "pc",
+            "when"    : when,
+            "ts"      : new Date().getTime()
+        }, loadProgramsStatus);
     });
 
     $("#cancel-power-btn").click(function() {
@@ -151,28 +180,40 @@ $(document).ready(function() {
         if (! confirm("Cancel event?")) {
             return;
         }
-        $.post('/programs', {"action":"clear", "program":"power"},
-            loadProgramsStatus);
+        $.post('/programs', {
+            "action"  : "clear",
+            "program" : "power",
+            "ts"      : new Date().getTime()
+        }, loadProgramsStatus);
     });
 
     /** Settings */
     $("#scheduler-select").change(function() {
-        $.post('/scheduler', {"name": $(this).val()}, function(data) {
+        $.post('/scheduler', {
+            "name" : $(this).val(),
+            "ts"   : new Date().getTime()
+        }, function(data) {
             alert(data['msg']);
         });
     });
     $("#ambient-switch").change(function() {
         disableUiUpdate();
         var active = $(this).val() == "on";
-        $.post('/lights', {"action": "set_ambient_active", "active": active}, 
-            loadLightsStatus);
+        $.post('/lights', {
+            "action" : "set_ambient_active",
+            "active" : active,
+            "ts"     : new Date().getTime()
+        }, loadLightsStatus);
     });
 
     $("#ambient-enable-switch").change(function() {
         disableUiUpdate();
         var enabled = $(this).val() == "on";
-        $.post('/lights', {"action": "set_ambient", "enable": enabled}, 
-            loadLightsStatus);
+        $.post('/lights', {
+            "action" : "set_ambient",
+            "enable" : enabled,
+            "ts"     : new Date().getTime()
+        }, loadLightsStatus);
     });
 
     $("#save-ambient-levels-btn").click(function() {
@@ -180,7 +221,8 @@ $(document).ready(function() {
         $.post('/lights', {
             "action"  : "save_ambient_levels", 
             "low"     : $("#low-light-txt").val(),
-            "maximum" : $("#max-light-txt").val()
+            "maximum" : $("#max-light-txt").val(),
+            "ts"      : new Date().getTime()
         }, function(data) {
             alert("Saved settings successfully.");
             loadLightsStatus(data);
